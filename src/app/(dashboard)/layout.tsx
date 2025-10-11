1b7464246dd1b138a96cebe744d4dd5cc0e372c3
Marvin Lim <marvinlim@Marvins-MacBook-Air.local>
2025-10-11 22:51:52 +0800

    theme toggle component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { AppSidebar } from "@/components/app-sidebar";
+import { ThemeToggle } from "@/components/theme-toggle";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -7,12 +9,16 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Toaster } from "sonner";
 
 export default function DashboardLayout({
@@ -20,6 +26,10 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
+  // split pathname: "/projects" -> ["projects"]
+  const segments = pathname.split("/").filter(Boolean);
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -30,19 +40,48 @@ export default function DashboardLayout({
             orientation="vertical"
             className="mr-2 data-[orientation=vertical]:h-4"
           />
+
+          {/* Breadcrumb */}
           <Breadcrumb>
             <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
+              {segments.length === 0 ? (
+                // fallback if user is at "/"
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Dashboard</BreadcrumbPage>
+                </BreadcrumbItem>
+              ) : (
+                segments.map((segment, index) => {
+                  const href = "/" + segments.slice(0, index + 1).join("/");
+                  const isLast = index === segments.length - 1;
+                  const label =
+                    segment.charAt(0).toUpperCase() + segment.slice(1);
+
+                  return (
+                    <BreadcrumbItem key={href}>
+                      {isLast ? (
+                        <BreadcrumbPage>{label}</BreadcrumbPage>
+                      ) : (
+                        <>
+                          <BreadcrumbLink asChild>
+                            <Link href={href}>{label}</Link>
+                          </BreadcrumbLink>
+                          <BreadcrumbSeparator />
+                        </>
+                      )}
+                    </BreadcrumbItem>
+                  );
+                })
+              )}
             </BreadcrumbList>
           </Breadcrumb>
+
+          <div className="ml-auto space-x-1">
+            <ThemeToggle />
+            <Button className="ml-auto" variant="ghost">
+              <LogOut />
+              Logout
+            </Button>
+          </div>
         </header>
 
         {/* Page Content */}
