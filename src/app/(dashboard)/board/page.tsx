26043ab884cf7de81e6e6f1cd5c1719034949d08
marvin <marvzknows03@gmail.com>
2025-10-17 16:57:24 +0800

    header UI component WIP

--- src/app/(dashboard)/board/page.tsx
+++ src/app/(dashboard)/board/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import BoardListComboBox from "./components/BoardListComboBox";
+import AddMemberDialog from "./components/AddMemberDialog";
+import AvatarStacked from "./components/AvatarStacked";
 
 const Board = () => {
-  return <div>Board</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="p-4 h-full flex flex-col border border-red-500">
+      {/* Board name & board list */}
+      <div className="flex items-center justify-between border-b pb-2">
+        <BoardListComboBox />
+        <div className="flex items-center gap-2">
+          <AvatarStacked />
+          <AddMemberDialog isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Board;
