e888e4439c484fe14db9b79884e4d74c6a7769b8
marvin <marvzknows03@gmail.com>
2025-10-17 14:01:34 +0800

    add firstname and last name for signing up

---
+++ prisma/migrations/20251017055623_reset_db/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `firstName` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `middleName` on the `user` table. All the data in the column will be lost.
+  - Made the column `name` on table `user` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "firstName",
+DROP COLUMN "lastName",
+DROP COLUMN "middleName",
+ALTER COLUMN "name" SET NOT NULL;
